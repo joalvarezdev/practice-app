029a1912f16000bec9242cb1bab66ceebf41c838
joalvarez <alvarez.joaquinez@gmail.com>
2024-02-20 17:19:37 -0300

    refactor: delete imports without use

--- example/src/main/java/com/joalvarez/example/config/security/SecurityConfig.java
+++ example/src/main/java/com/joalvarez/example/config/security/SecurityConfig.java
@@ -3,11 +3,8 @@ package com.joalvarez.example.config.security;
 import com.joalvarez.example.config.security.constants.SecurityProperties;
 import com.joalvarez.example.config.security.jwt.JwtAuthenticationFilter;
 import com.joalvarez.example.config.security.jwt.JwtValidationFilter;
-import io.swagger.v3.oas.models.PathItem;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -18,14 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
