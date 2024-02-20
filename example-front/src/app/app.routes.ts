7332a1b857bdb3466d114e8c9c804656de539097
joalvarez <alvarez.joaquinez@gmail.com>
2024-02-20 14:12:51 -0300

    feat: add principal layouts and views

--- example-front/src/app/app.routes.ts
+++ example-front/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './shared/layouts/principal/principal.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: PrincipalComponent,
+    children: [
+      {
+        path: '/home',
+        component: HomeComponent,
+      },
+    ],
+  },
+];
