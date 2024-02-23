d4e1c96fdac3475fe4e093d9cb5c322abac96385
joalvarez <alvarez.joaquinez@gmail.com>
2024-02-22 22:02:39 -0300

    feat: add products resources

---
+++ example/src/main/resources/database/ddl/04-products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL NOT NULL,
+    sku VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    user_id UUID NOT NULL UNIQUE,
+    CONSTRAINT "pk_products" PRIMARY KEY (id)
+);
